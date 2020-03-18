62ce950de6d8cc3dc4e2756ff4bf08bbfeba7366
Sam Texas <sam@simplecto.com>
2020-03-18 19:35:00 +0100

    added a simple robots.txt setup

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    path('robots.txt',
+         TemplateView.as_view(template_name="robots.txt", content_type='text/plain'),
+         name="robots-txt"),
     path('admin/', admin.site.urls),
     path('', views.index, name="home"),
     path('health-check', views.health_check, name='health-check'),
