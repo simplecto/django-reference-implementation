f07bc44d51d2c63d679b6636e135080836fde2b0
Sam Texas <undernewmanagement@users.noreply.github.com>
2023-05-04 22:27:26 +0200

    tweaks for sentry

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,14 +13,15 @@ load_dotenv(find_dotenv(os.getenv('ENV_FILE', 'env')))
 DEBUG = os.environ['DEBUG']
 SECRET_KEY = os.environ['SECRET_KEY']
 
-if os.environ['DEV_ENV'] in ['production', 'staging']:
+if os.environ['DEV_ENV'] in ['production']:
     sentry_sdk.init(
         dsn=os.environ['SENTRY_DSN'],
         integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
+        send_default_pii=False
     )
 
 # You can explicitly turn this off in your env file
