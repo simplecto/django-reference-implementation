35f15d015b5dde33e7a80319003a0aa83c92d86d
Sam Texas <undernewmanagement@users.noreply.github.com>
2024-04-11 18:01:24 +0200

    added template worker and table to manege it

---
+++ src/myapp/management/commands/simple_async_worker.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+from myapp.models import SiteConfiguration
+import time
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    help = 'Simple Async Worker'
+
+    def __init__(self):
+        super().__init__()
+        self.config = SiteConfiguration.get_solo()
+
+    def handle(self, *args, **options):
+
+        logger.info("Starting job...")
+
+        while True:
+            self.config.refresh_from_db()
+
+            if self.config.worker_enabled:
+                logger.info(f"Running the job.")
+            else:
+                logger.info(f"Job is disabled.")
+
+            logger.info(f"Sleeping for {self.config.worker_sleep_seconds} seconds.")
+            time.sleep(self.config.worker_sleep_seconds)
--- src/myapp/models.py
+++ src/myapp/models.py
@@ -1,3 +1,14 @@
+from solo.models import SingletonModel
 from django.db import models
 
-# Create your models here.
+
+FIVE_SECONDS = 5
+
+
+class SiteConfiguration(SingletonModel):
+
+    worker_enabled = models.BooleanField(default=False)
+    worker_sleep_seconds = models.IntegerField(default=FIVE_SECONDS)
+
+    def __str__(self):
+        return 'Site Configuration'
